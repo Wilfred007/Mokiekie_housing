ede2fa668c52bff146d3c055bb81e37282a513e1
Wilfred007 <adzerwilfred007@gmail.com>
2024-07-04 04:04:53 -0700

    nav

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -11,17 +11,17 @@ const Header = () => {
     console.log(path)
   },[])
   return (
-    <div className='flex justify-between p-6 px-10 shadow-sm fixed top-0 w-full z-10 bg-white'>
+    <div className='flex justify-between p-6 px-10 shadow-sm gap-3 fixed top-0 w-full z-10 bg-white'>
       <div className='flex gap-1'>
-        <h1 className='font-bold text-2xl text-slate-800'>Mokiekie</h1>
-        <House className='w-5 h-5'/>
-        <h1>Global Ltd</h1>
+        <h1 className='font-bold text-1xl text-slate-800'>Mokiekie</h1>
+        <h1 className=''>Global Ltd</h1>
       </div>
+      {
       <ul className='flex gap-5'>
         <Link href={'/'}><li className={`'text-sm text-gray-700 ${path=='/'&&'text-green-500 text-sm'}`}>For Sell</li></Link>
         <li className='text-sm text-gray-700'>For Rent</li>
-        <li className='text-sm text-gray-700'>About Us</li>
       </ul>
+      }
       
     </div>
   )
